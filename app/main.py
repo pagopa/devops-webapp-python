b37078ccebf692eb419b6fcda210be6d8e958b42
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-02-22 08:30:07 +0100

    added config map & secrets in env print

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from ast import If
+import json
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,6 +10,17 @@ app = FastAPI()
 async def root():
     return {"root": "ok"}
 
+@app.get("/api")
+async def api():
+    my_configmap_value = os.getenv("MY_CONFIGMAP_VALUE")
+    my_secret_value = os.getenv("MY_SECRET_VALUE")
+
+    api_values = {
+        "my_configmap_value" : my_configmap_value,
+        "my_secret_value" : my_secret_value
+    }
+
+    return api_values
 
 @app.get("/status")
 async def status():
